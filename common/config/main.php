c3ff34851aa762ba84d93ad3701e3282b73492ff
amor <persoomar@gmail.com>
2016-11-04 15:32:35 +0100

    test git capability

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,11 @@
 <?php
 return [
+    'name' => 'Trombi.OH',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
     ],
+
 ];
